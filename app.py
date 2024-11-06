11a1056934d2e32abe13a6325353f50287b6b727
Yangbo-Rai <yangbo.rai@y-tech.co.uk>
2024-11-06 21:00:38 +0000

    Update app.py

--- app.py
+++ app.py
@@ -21,5 +21,5 @@ def get_info():
         "specialization": "Front-end Technology"
     })
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+#if __name__ == '__main__':
+#    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
