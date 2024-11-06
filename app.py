5bf00e7453d0605ef166603fb0f0fb882d44895f
Yangbo-Rai <yangbo.rai@y-tech.co.uk>
2024-11-06 19:54:13 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/api/info')
+def get_info():
+    return jsonify({
+        "name": "Yangbo Rai",
+        "title": "Full Stack Developer",
+        "specialization": "Front-end Technology"
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
